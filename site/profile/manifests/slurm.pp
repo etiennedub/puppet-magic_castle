8e5a279d4c79e6a7d93c366c19daad64c84f46a3
etienned <etienne.dubeau@calculquebec.ca>
2026-03-31 08:43:45 -0400

    Add tfe_api_url support

--- site/profile/manifests/slurm.pp
+++ site/profile/manifests/slurm.pp
@@ -408,6 +408,7 @@ class profile::slurm::controller (
   String $tfe_token = '',
   String $tfe_workspace = '',
   String $tfe_var_pool = 'pool',
+  Optional[String] $tfe_api_url = undef,
 ) {
   contain profile::slurm::base
 
@@ -441,15 +442,22 @@ class profile::slurm::controller (
     ],
   }
 
+  $tfe_api_url_export = $tfe_api_url ? {
+    undef   => '',
+    default => "export TFE_API_URL=${tfe_api_url}\n",
+  }
+
+  $env_secrets_base = @("EOT")
+export TFE_TOKEN=${tfe_token}
+export TFE_WORKSPACE=${tfe_workspace}
+export TFE_VAR_POOL=${tfe_var_pool}
+EOT
+
   file { '/etc/slurm/env.secrets':
     ensure  => 'present',
     owner   => 'slurm',
     mode    => '0600',
-    content => @("EOT")
-export TFE_TOKEN=${tfe_token}
-export TFE_WORKSPACE=${tfe_workspace}
-export TFE_VAR_POOL=${tfe_var_pool}
-|EOT
+    content => "${env_secrets_base}${tfe_api_url_export}",
   }
 
   file { '/usr/bin/slurm_resume':
